609f0187e45d8ee810a3dbcd032f5abeb4a4cd5b
Ziv Serphos <zivfromisrael@gmail.com>
2021-10-28 18:31:22 +0300

    updated class View in development

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ class Model {
         this.index = 0;
     }
 
-    set board(arr) {
-        const [x,y] = arr;
-        if (this.index % 2 === 0) {
-            this.board[x][y] = "RED"
-            return this.board
-        }
-        this.board[x][y] = "BLUE"
-        return this.board    
-    }
+    // set board(arr) {
+    //     const [x,y] = arr;
+    //     if (this.index % 2 === 0) {
+    //         this.board[x][y] = "RED"
+    //         return this.board
+    //     }
+    //     this.board[x][y] = "BLUE"
+    //     return this.board    
+    // }
     
 }
 
 class View{
     constructor() {
-
+        this.board = this.defaultBoard()
     }
     defaultBoard() {
         const root = document.getElementById("root")
@@ -28,13 +28,20 @@ class View{
         }
 
     }
-    createElement(tag, className) {
-        const element = document.createElement(tag);
+    createElement(tagName, children = [], classes = [], attributes = {}) { // create new element in more comfortable
+        const el = document.createElement(tagName); 
+        for (let child of children) { // append childs of element
+            el.append(child)
+        }
+        for (let cls of classes) { // add all the classes to the element
+            el.classList.add(cls)
+        }
+        for (let attr in attributes) { // add all attributes to the element
+            el.setAttribute(attr , attributes[attr])
+        }
+        return el
+    }
     
-        if (className) element.classList.add(className);
-    
-        return element;
-      }
     
 }
 
